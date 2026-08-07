bf93c4b5db8e53756fa88612aa1f71e961bdb31d
William Nata Silva Mascarenhas <william29370726@aluno.ba.senac.br>
2026-08-06 21:05:54 -0300

    Criação de rotas de login e recuperação de senha

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/login', function () {
+    return view('login.loginP');
+});
+Route::get('/novasenha', function () {
+    return view('login.novasenha');
+});
+Route::get('/recuperacao', function () {
+    return view('login.recuperacaoP');
+});
