9f91861ecb4dace1ed335877621f384e2ad920d8
Isabela Cristina dos Santos <isabela2994746@aluno.ba.senac.br>
2026-08-07 19:42:40 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/teste', function () {
+    return view('pesquisa.teste');
+});
+
+
+Route::get('/feedback', function () {
+    return view('pesquisa.feedback');
+});
+
+Route::get('/satisfacaocliente', function () {
+    return view('pesquisa.satisfacaocliente');
+});
+
+Route::get('/cancelado', function () {
+    return view('components.cancelado');
+});
+
+Route::get('/confirmacaoagendamento', function () {
+    return view('components.confirmacaoagendamento');
+});
+
+Route::get('/confirmado', function () {
+    return view('components.confirmado');
+});
+
+
+Route::get('/teste', function () {
+    return view('pesquisa.teste');
+});
