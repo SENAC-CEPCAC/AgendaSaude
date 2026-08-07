862a436e81282dcbf8e2a5a60bee94f3f2f5fb8d
William Nata Silva Mascarenhas <william29370726@aluno.ba.senac.br>
2026-08-07 20:31:23 -0300

    Baixar att

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
-    return view('login.loginP');
+    return view('login.login');
 });
+
 Route::get('/novasenha', function () {
     return view('login.novasenha');
 });
 Route::get('/recuperacao', function () {
-    return view('login.recuperacaoP');
-});
-Route::get('/', function () {
+    return view('login.recuperacao');
+})->name('login.recuperacao');
+
+Route::get('/listaAgendamento', function () {
     return view('ListaAgendamento.listaAgendamento');
+});
+Route::get('/cadastro', function () {
+    return view('login.cadastro');
 });
