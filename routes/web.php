2b7def8bc4e6a1ba91800dcc681b00a5357f354d
Joao Moraes <joao.moraes@ba.senac.br>
2026-08-10 18:33:52 -0300

    atualização 1.0

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::get('/teste', function () {
     return view('pesquisa.teste');
 });
@@ -32,17 +31,3 @@ Route::get('/confirmado', function () {
 Route::get('/teste', function () {
     return view('pesquisa.teste');
 });
-=======
-Route::get('/login', function () {
-    return view('login.loginP');
-});
-Route::get('/novasenha', function () {
-    return view('login.novasenha');
-});
-Route::get('/recuperacao', function () {
-    return view('login.recuperacaoP');
-});
-Route::get('/', function () {
-    return view('ListaAgendamento.listaAgendamento');
-});
->>>>>>> d093cdf2d3b5f4bf1123aab3808cd17f695a5568
