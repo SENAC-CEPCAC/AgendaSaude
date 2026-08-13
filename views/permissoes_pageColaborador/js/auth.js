322c05c37acc3a9c806e5aad6536a4064b4aa53a
Rafael Conceicao Nascimento <rafael54377866@aluno.ba.senac.br>
2026-08-13 19:03:34 -0300

    melhoria index e login

--- views/permissoes_pageColaborador/js/auth.js
+++ views/permissoes_pageColaborador/js/auth.js
@@ -95,11 +95,7 @@ function validarAcesso(nivelMinimoRequerido) {
         break;
       case 1:
       default:
-<<<<<<< HEAD
-        window.location.href = 'home.html'; 
-=======
         window.location.href = 'index.html'; 
->>>>>>> 0690607421999b3f924124b84085365697bd09bd
         break;
     }
   }
@@ -107,9 +103,5 @@ function validarAcesso(nivelMinimoRequerido) {
 
 function logout() {
   localStorage.removeItem('userLogado');
-<<<<<<< HEAD
-  window.location.href = getHomePageUrl();
-=======
   window.location.href = 'index.html';
->>>>>>> 0690607421999b3f924124b84085365697bd09bd
 }
