3dfcdc39889c496e7f8dd9ec6a8c941b3f9825a2
gabriel-aragao-app <g.aragao.app@gmail.com>
2026-08-14 12:01:45 -0300

    Crie as migrations e seedres

---
+++ database/migrations/0010_fato_prontuario.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * PSEUDOCÓDIGO DE EXECUÇÃO:
+     * 1. CRIE A TABELA 'fato_prontuario'.
+     * 2. DEFINA 'id_prontuario' COMO CHAVE PRIMÁRIA.
+     * 3. VINCULE 'id_paciente' COM 'dim_pacientes' E 'id_agenda' COM 'fato_cronogramas'.
+     * 4. ADICIONE 'status_comparecimento' (ENUM: 'agendado', 'presente', 'faltou', 'cancelado').
+     */
+    public function up(): void
+    {
+        Schema::create('fato_prontuario', function (Blueprint $table) {
+
+            $table->increments('id_prontuario');
+
+            $table->unsignedInteger('id_paciente');
+            
+            $table->foreign('id_paciente')
+                    ->references('id_paciente')
+                    ->on('dim_pacientes')
+                    ->onDelete('cascade');
+
+            $table->unsignedInteger('id_agenda');
+
+            $table->foreign('id_agenda')
+                    ->references('id_agenda')
+                    ->on('fato_cronogramas')
+                    ->onDelete('cascade');
+
+            $table->enum('status_comparecimento', ['agendado', 'presente', 'faltou', 'cancelado'])->default('agendado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fato_prontuario');
+    }
+};
