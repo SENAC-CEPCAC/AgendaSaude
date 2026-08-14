c3c5a3582355b56b8eaf1ec917abadd98a2321b3
Rafael Conceicao Nascimento <rafael54377866@aluno.ba.senac.br>
2026-08-14 20:58:47 -0300

    perfumaria no layout

--- resources/views/permissao_colaborador/novasenha.blade.php
+++ resources/views/permissao_colaborador/novasenha.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nova senha</title>
     
     <link rel="stylesheet" href="/css/perfilPaciente.css">
 
@@ -18,22 +18,21 @@
 </head>
 <body class="bg-[#f8fafc]">
 
-    <div class="main flex justify-center items-center h-screen ">
+    <div class="main flex justify-center items-center h-screen">
 
-        <div class="border border-slate-50 bg-white w-80 h-90 shadow-sm p-10 ">
-        <h2 class="text-3xl font-bold">Nova Senha</h2>
+        <div class="border border-slate-50 bg-white w-80 h-90 p-13 rounded-xl shadow-md ">
+        <h2 class="text-3xl  ">Nova Senha</h2>
         
         <div>
-            <h4 class="email text-sm ml-1">Digite uma nova senha:</h4>
-            <input class="input input border-1 border-gray-300 rounded-md pl-3" type="password" name="password" id="password"
-                placeholder="Insira a nova senha">
-            
-                
+            <h4 class="email pt-5 text-sm ml-1">Digite uma nova senha:</h4>
+            <input class="input border-1 border-gray-300 rounded-md pl-10" type="password" name="password" id="password"
+                placeholder="Insira a nova senha">            
+            <br>
              <h4 class="text-sm mt-4 ml-1">Digite novamente:</h4>
-            <input class="input border-1 border-gray-300 rounded-md pl-3" type="password"  name="password" id="password"
+            <input class="input  border-1 border-gray-300 rounded-md pl-10" type="password"  name="password" id="password"
                 placeholder="Insira novamente">
-
-            <button class="butao bg-clip-border px-19 py-1 bg-blue-900 text-white rounded-lg mt-2" type="submit">Confirmar</a></button>
+            <br><br>
+            <button class="butao font-bold cursor-pointer hover:bg-blue-700 bg-clip-border px-19 py-1 bg-blue-500 text-white rounded-lg mt-2" type="submit">Confirmar</a></button>
             </div>
         </div>
 </body>
