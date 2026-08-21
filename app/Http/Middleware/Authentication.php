2c9b93265c00b488bb98edc62561b0f6bde706dc
rafaelnascimento1986 <rafael.conceicao.nascimento1986@gmail.com>
2026-08-21 13:53:27 -0300

    atualizacao de acesso e permissao

--- app/Http/Middleware/Authentication.php
+++ app/Http/Middleware/Authentication.php
@@ -2,22 +2,21 @@
 
 namespace App\Http\Middleware;
 
-use closure;
+use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
-use lluminate\Auth\AutheticationException;
 
 class Authentication
 {
-    public function handle( Request $request, Closure $next, $niveis): Response
+    public function handle(Request $request, Closure $next, ...$niveis): Response
     {
         if(!Auth::check())
         {
-            throw new AuthenticationException();
+            return redirect()->guest(route('acesso.login'));
         }
 
-        if(!in_array(Auth::user()->nivel, $niveis))
+        if (!in_array((int) Auth::user()->nivel, array_map('intval', $niveis), true))
         {
             abort(403, 'Acesso negado, você não tem permissão para acessar esta página. Autentique-se!');
         }
--- routes/web.php
+++ routes/web.php
@@ -8,93 +8,122 @@ use App\Http\Controllers\AgendamentoEtapa3Controller;
 use App\Http\Controllers\AnamneseColoController;
 use App\Http\Controllers\AnamneseMamaController;
 use App\Http\Controllers\ListaAgendamentoController;
+use App\Http\Controllers\LoginController;
 
 
 
 // PAINEL PACIENTE
 
-Route::get('/login', function () { //WILLIAM
-    return views('login.loginP');
-});
-
-Route::get('/teste', function () { //WILLIAM
-    return views('pesquisa.teste');
-});
-
-Route::get('/novasenha', function () { //WILLIAM
-    return views('login.novasenha');
-});
-Route::get('/recuperacao', function () { //WILLIAM
-    return views('login.recuperacaoP');
-});
-
-route::get('/agendamento/etapa-1', [AgendamentoEtapa1Controller::class, 'index'])->name('agendamento.etapa1'); //GABRIEL
-
-route::get('/agendamento/etapa-2', [AgendamentoEtapa2Controller::class, 'index'])->name('agendamento.etapa2'); //GABRIEL
-
-route::get('/agendamento/etapa-3', [AgendamentoEtapa3Controller::class, 'index'])->name('agendamento.etapa3'); //GABRIEL
-
-
 Route::get('/feedback', function () {
-    return views('pesquisa.feedback'); //ISABELA
+    return view('pesquisa.feedback'); //ISABELA
 });
 
 //Route::get('/satisfacaocliente', function () {
-//return views('pesquisa.satisfacaocliente');//ISABELA
+//return view('pesquisa.satisfacaocliente');//ISABELA
 //});
 
 Route::get('/cancelado', function () {
-    return views('components.cancelado'); //ISABELA
+    return view('components.cancelado'); //ISABELA
 });
 
 Route::get('/confirmacaoagendamento', function () {
-    return viewss('components.confirmacaoagendamento'); //ISABELA
+    return view('components.confirmacaoagendamento'); //ISABELA
 });
 
 Route::get('/confirmado', function () {
-    return views('components.confirmado'); //ISABELA
+    return view('components.confirmado'); //ISABELA
 });
 
-
 Route::get('/teste', function () {
-    return views('pesquisa.teste'); //ISABELA
+    return view('pesquisa.teste'); //ISABELA
 });
 
 Route::get('/colo', function () {
-    return views('anamnese.colo');
+    return view('anamnese.colo');
 });
 
 Route::get('/mama', function () {
-    return views('anamnese.mama');
+    return view('anamnese.mama');
 });
 
 Route::get('/unidadesmoveis', function () {
-    return views('anamnese.unidadesmoveis');
+    return view('anamnese.unidadesmoveis');
 });
-
-
 
 Route::get('/', function () {
-    return views('acesso.index'); //RAFAEL
+    return view('acesso.index'); //RAFAEL
 });
 
+//RAFAEL
 Route::get('/login', function () {
-    return views('acesso.login'); //RAFAEL
+    return view('acesso.login');
 })->name('acesso.login');
 
+Route::post('/login', [LoginController::class, 'logar'])->name('login.attempt');
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
+
 Route::get('/cadastro', function () {
-    return views('acesso.cadastro'); //RAFAEL
+    return view('acesso.cadastro'); //RAFAEL
 })->name('acesso.cadastro');
 
 Route::get('/novasenha', function () {
-    return views('recuperacao.novasenha'); //RAFAEL
+    return view('recuperacao.novasenha'); //RAFAEL
 })->name('recuperacao.novasenha');
 
 Route::get('/recuperacao', function () {
-    return views('recuperacao.recuperacao'); //RAFAEL
+    return view('recuperacao.recuperacao'); //RAFAEL
 })->name('recuperacao.recuperacao');
 
-Route::get('/colaborador', function () {
-    return views('colaborador.colaborador');
+Route::get('/index', function () {
+    return view('acesso.index');
 });
 
+Route::get('/index.php', function () {
+    return view('acesso.index');
+});
+
+//ACESSO AOS COLABORADORES DE NIVEL - 2
+Route::middleware('auth.nivel:2')->group(function () {
+    // N2
+    Route::get('/colaborador', function () {
+        return view('colaborador.colaborador');
+    });    
+});
+
+//ACESSO AOS COLABORADORES DE NIVEL - 4
+Route::middleware('auth.nivel:4')->group(function () {    
+    // N4
+    Route::get('/acesso_restrito', function () {
+        return view('acesso_restrito.acesso_restrito');
+    })->name('acesso_restrito.acesso_restrito');
+});
+
+
+// Autenticação
+Route::middleware('auth.nivel:1,2,3')->group(function () {
+    // N1
+    Route::post('/agendasaude1', [AgendamentoEtapa1Controller::class, 'AgendamentoEtapa1Controller'])->name('agendamento.etapa1'); //WILLIAM
+
+    Route::post('/agendasaude2', [AgendamentoEtapa2Controller::class, 'AgendamentoEtapa2Controller'])->name('agendamento.etapa2'); //WILLIAM
+
+    Route::post('/agendasaude3', [AgendamentoEtapa3Controller::class, 'AgendamentoEtapa3Controller'])->name('agendamento.etapa3'); //WILLIAM
+    // N2 
+
+    // N3 
+
+    // N4
+
+
+
+    //Route::post('/imcCalcular', [ImcController::class, 'calcularImc'])->name('imc.calcular');
+
+    //Route::post('/imc', [ImcController::class, 'store'])->name('imc.store');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dash.index');
+    Route::delete('/dashboard/delete/{id}', [DashboardController::class, 'destroy'])->name('dash.delete');
+    Route::put('/dashboard/update/{id}', [DashboardController::class, 'update'])->name('dash.update');
+});
+
+Route::get('/agendamentos-gestao', [ListaAgendamentoController::class, 'index'])->name('agendamentos.index'); // Mateus
+Route::get('/agendamentos/{id}', [ListaAgendamentoController::class, 'show'])->name('agendamentos.show'); // Mateus
+Route::post('/agendamentos/{id}/validar-documento', [ListaAgendamentoController::class, 'validarDocumentos'])->name('agendamentos.validar-documento'); // Mateus
